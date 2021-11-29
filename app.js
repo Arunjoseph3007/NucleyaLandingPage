4813333b281104b098fb460d674e7007000cb275
Arunjoseph3007 <92107769+Arunjoseph3007@users.noreply.github.com>
2021-11-29 16:19:36 +0530

    Final

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-window.onload = function() {
-    setTimeout(()=>{
-	document.querySelector("#loader").style.opacity = 0;
-	document.querySelector("#loader").style.display = "none";
-	document.querySelector("#loader").style.pointerEventa = "none";
-    },2000)
-};
+gsap.from(".main-title",{duration:1,y:-25,opacity:0,ease:"bounce"})
+gsap.from(".banner-image",{duration:0.8,x:30,opacity:0,ease:"power2",delay:0.7})
